be949898ed38d91b8fe1020b096d1ad93781f8dc
Charlie <charlieryancurry@gmail.com>
2023-11-15 14:29:59 -0400

    finish test

--- ReadingWritingFiles/Program.cs
+++ ReadingWritingFiles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ReadingWritingFiles
 {
@@ -10,6 +11,36 @@ namespace ReadingWritingFiles
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Reading / Writing Files");
+            Console.WriteLine();
+
+            //read file
+            string path = @"test.txt";
+            string input;
+            input = File.ReadAllText(path);
+            Console.WriteLine(input);
+
+            Console.WriteLine();
+
+            string[] mapRows;
+            mapRows = File.ReadAllLines(path);
+            for (int y = 0; y < mapRows.Length; y++)
+            {
+                string mapRow = mapRows[y];
+                //Console.WriteLine(mapRow);
+                for (int x =  0; x < mapRow.Length; x++)
+                {
+                    char tile = mapRow[x];
+                    Console.Write(tile);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+
         }
     }
 }
